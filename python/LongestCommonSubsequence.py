e5425a6ccb92190417224516788d8931ba8af97c
Lance <Lanceyshi@gmail.com>
2021-09-26 13:43:17 +1000

    Add longest common subsequence solution

---
+++ python/LongestCommonSubsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        lcs = [[0 for i in range(n+1)] for i in range(m+1)]
+        for i in range(m):
+            for j in range(n):
+                if text1[i] == text2[j]:
+                    lcs[i+1][j+1] = lcs[i][j] + 1
+                else:
+                    lcs[i+1][j+1] = max(lcs[i][j+1], lcs[i+1][j])
+        return lcs[i+1][j+1]
