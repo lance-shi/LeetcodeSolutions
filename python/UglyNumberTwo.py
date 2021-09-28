18b480afdadea2143d37b20060a352ab96371134
Lance <Lanceyshi@gmail.com>
2021-09-28 12:58:25 +1000

    Add ugly number two

---
+++ python/UglyNumberTwo.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly_list = [1]
+        i2 = i3 = i5 = 0
+        i = 1
+        while i < n:
+            new_num = min(ugly_list[i2] * 2, ugly_list[i3] * 3, ugly_list[i5] * 5)
+            ugly_list.append(new_num)
+            if new_num == ugly_list[i2] * 2:
+                i2 += 1
+            if new_num == ugly_list[i3] * 3:
+                i3 += 1
+            if new_num == ugly_list[i5] * 5:
+                i5 += 1
+            i += 1
+        return ugly_list[-1]
