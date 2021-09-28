e3d85f908ca83c2188be35148d914b2019685d5c
Lance <Lanceyshi@gmail.com>
2021-09-28 12:11:53 +1000

    Add ugly number and unique email address

---
+++ python/UglyNumber.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while not n % 5:
+            n = n // 5
+        while not n % 3:
+            n = n // 3
+        while not n % 2:
+            n = n // 2
+        return n == 1
