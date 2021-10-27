7d9b898825970d0cda84751fe46aa77ae2bfccee
Lance <Lanceyshi@gmail.com>
2021-10-27 17:26:31 +1100

    Add sort colors solution

---
+++ python/SortColors.py
@@ -0,0 +1,17 @@
+# Question No. 75
+# Sort Colors
+class Solution:
+    def sortColors(self, nums) -> None:
+        r = g = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                r += 1
+                if nums[r - 1] > 0:
+                    nums[r - 1], nums[i] = nums[i], nums[r - 1]
+                if g != 0:
+                    nums[r + g - 1], nums[i] = nums[i], nums[r + g - 1]
+                continue
+            if nums[i] == 1:
+                g += 1
+                if nums[r + g - 1] > 1:
+                    nums[r + g - 1], nums[i] = nums[i], nums[r + g - 1]
